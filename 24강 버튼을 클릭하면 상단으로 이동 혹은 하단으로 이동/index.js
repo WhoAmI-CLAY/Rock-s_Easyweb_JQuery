0cb2ce9cbf0ed446aafacaff9fe8a7edb3a589c6
CLAY <82664613+WhoAmI-CLAY@users.noreply.github.com>
2021-05-12 00:37:23 +0900

    버튼을 누르면 맨 꼭대기 또는 맨 하단으로 이동한다.

---
+++ 24강 버튼을 클릭하면 상단으로 이동 혹은 하단으로 이동/index.js
@@ -0,0 +1,23 @@
+$(function(){
+    let target = $('.side').find('button')
+    
+    $(window).scroll(function(){
+        if($(this).scrollTop()  == 0){
+            $("i").attr("class","fas fa-caret-down")
+        }
+        else{
+            $("i").attr("class","fas fa-caret-up")
+        }
+    })
+
+    $(target).click(function() {
+        if($(window).scrollTop() == 0){
+            //맨 밑으로 이동하는 코드
+            $('html , body').animate({scrollTop:$('.footer_location').offset().top}, 300);
+        }
+        else{
+            //맨 아래로 이동하는 코드
+            $('html , body').animate({scrollTop:0}, 300);
+        } 
+    })
+})
